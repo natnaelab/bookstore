bbc91a23ef21987f66f4e3d0ac182b5be62108d0
Nati <natnael08@protonmail.com>
2020-11-01 17:42:42 +0300

    added sign up feature (chapter 5)

--- project/urls.py
+++ project/urls.py
@@ -4,5 +4,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),  # django admin
     path('', include('pages.urls')),  # local apps
-    path('accounts/', include('django.contrib.auth.urls'))  # user management
+    path('accounts/', include('django.contrib.auth.urls')),  # user management
+    path('accounts/', include('users.urls'))  # user signup
 ]
